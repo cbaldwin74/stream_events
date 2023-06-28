acb42c20bf37be0a3c857aa4f3f0660ecfe082b2
Chris Baldwin <cbaldwin@shaw.ca>
2023-06-28 08:57:45 -0700

    Add a comment with a detailed explanation of the query used to provide the event list

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -12,6 +12,60 @@ use Illuminate\Support\Facades\DB;
 
 class EventController extends Controller
 {
+    /**
+     * TLDR: Overall, the query retrieves relevant data from multiple tables for a given user ID 
+     * and combines them into a single result set, handling missing values gracefully using 
+     * the `COALESCE` function.
+     * 
+     * Details:
+     * 
+     * This SQL query retrieves data from multiple tables (`donations`, `followers`, `merch_sales`, `subscribers`) 
+     * based on a given user ID and combines the results into a single result set. 
+     * Here's a breakdown of the query:
+     * 
+     * 1. The `SELECT` statement is used to select the columns that will be included in the result set. 
+     *    The `COALESCE` function is used to select the first non-null value from a list of columns. 
+     * 2. The `FROM` clause includes a subquery that combines the data from all the tables using the 
+     *   `UNION` operator. This subquery assigns a source value to each row indicating which table 
+     *    the data originated from.
+     * 3. The `LEFT JOIN` statements join the subquery (`ad`) with each individual table based on 
+     *    the user ID and the corresponding source value. This ensures that all rows from the 
+     *    subquery are included in the result set, even if there are no matches in the joined tables.
+     * 4. The `WHERE` clause filters the rows based on a specific user ID, represented by the placeholder `?`.
+     * 
+     * Now let's go through the selected columns:
+     * 
+     * - `id`: The `COALESCE` function is used to select the first non-null value from the `id` 
+     *   column of each table. It prioritizes the order of selection as `donations.id`, `followers.id`,
+     *   `merch_sales.id`, and `subscribers.id`.
+     * 
+     * - `name`: Similar to `id`, it selects the first non-null value from the `name` column of each table.
+     * 
+     * - `read`: Selects the first non-null value from the `read` column of each table.
+     * 
+     * - `amount`: Selects the first non-null value from the `amount` column of the `donations` 
+     *   table and the `price` column of the `merch_sales` table. The `NULL` value is used as a 
+     *   placeholder for the columns that are not relevant to a particular table.
+     * 
+     * - `currency`: Selects the first non-null value from the `currency` column of the `donations` 
+     *   table and the `currency` column of the `merch_sales` table.
+     * 
+     * - `item`: Selects the `item` column from the `merch_sales` table. It ignores 
+     *   the `NULL` placeholders from other tables.
+     * 
+     * - `count`: Selects the `count` column from the `merch_sales` table. It ignores 
+     *   the `NULL` placeholders from other tables.
+     * 
+     * - `message`: Selects the `message` column from the `donations` table. It ignores 
+     *   the `NULL` placeholders from other tables.
+     * 
+     * - `tier`: Selects the `tier` column from the `subscribers` table. It ignores the 
+     *   `NULL` placeholders from other tables.
+     * 
+     * - `event_time`: Selects the first non-null value from the `event_time` column of each table.
+     * 
+     * Adapted from https://stackoverflow.com/questions/28349354/sorting-and-merging-data-from-three-different-tables-in-an-efficient-way
+     */
     private const STREAM_QUERY = <<<EOT
 COALESCE(donations.id, followers.id, merch_sales.id, subscribers.id) id,
     COALESCE(donations.name, followers.name, merch_sales.name, subscribers.name) 'name',
@@ -38,7 +92,7 @@ LEFT JOIN merch_sales on ad.user_id = merch_sales.user_id AND ad.source = 'merch
 LEFT JOIN subscribers on ad.user_id = subscribers.user_id AND ad.source = 'subscribers'
 WHERE ad.user_id = ?
 EOT;
-//order by event_time DESC limit 100;
+
     private const SUB_REVENUE= [500, 1000, 1500];
 
     /**
